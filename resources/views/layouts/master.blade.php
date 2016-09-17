f5b020ae88724eafa75fcb69113399c2a2600de8
candra <candragctk17@gmail.com>
2016-09-17 11:49:53 +0700

    Login form

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/custom.css">
-        
+        <link href="css/stylelogin.css" rel='stylesheet' type='text/css' />    
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:300,500,500i,700,900|Sail" rel="stylesheet"> 
         @yield('head')
 
         <title>Qrim - @yield('title')</title>
