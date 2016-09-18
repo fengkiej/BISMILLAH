99ce1cfe7b96c17693a116c9019da82b8d1be59e
raihanyuwono <raihanyuwono@hotmail.com>
2016-09-18 07:13:19 +0700

    add

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content')
-	<div class="card main" style="text-align: center; background-color: #fafafa;">
+	<div class="card" style="text-align: center; background-color: #fafafa; margin : 50px 10px 50px 10px;">
 		<div style="background-image: "../images/gallery-8.jpg";" class="profile-header img-responsive">
 			<span>
 			<img src="images/team-5.png" alt="User" class="img-circle img-responsive" style="width: 20vh; height: 20vh; background-color:#f6f6f6; margin-left:auto; margin-right:auto;">
@@ -20,27 +20,11 @@
 		<button class="btn btn-success" style="margin:5px; width: 9em; text-decoration: none;">Qrim</button>
 		</form>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-		<div class="row" style="margin-left:auto; margin-right:auto; width:80%; border:2px solid #ddd; margin-bottom: 20px;">
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 5c2c3ced49ee3ee424407ea15f7987327cf0ede5
 		<div class="row" style="margin-left:auto; margin-right:auto; width:95%; border:2px solid #ddd">
 			<div style="width:100%; height:275px; border-top:1px solid #ddd; border-bottom:1px solid #ddd; overflow-y:scroll; background:white">
 				
 			</div>
-<<<<<<< HEAD
-=======
-		<div class="row" style="margin-left:auto; margin-right:auto; width:80%; border:2px solid #ddd">
->>>>>>> b5df79b527239f9964b952eeb00e5ce546bbfa19
-			<div>Welcome</div>
-			<div style="width:100%; height:275px; border-top:1px solid #ddd; border-bottom:1px solid #ddd; overflow-y:auto; background:white"></div>
->>>>>>> 1e5b1ad1480a79d95de16efc0ab270417cc90c27
-=======
 
->>>>>>> 5c2c3ced49ee3ee424407ea15f7987327cf0ede5
 			<div class="row" style="width:100%; height:30px; margin-left:auto; margin-right:auto; position:inline-block;">
 				<form>
 					<input type="text" name="text" style="height:30px; width:75%; margin:0px;"><!--
